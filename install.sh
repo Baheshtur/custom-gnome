6640a013b9fa6e916c1a15696f16d050ea3b0bf1
eudescar <eudes.scar@gmail.com>
2017-08-04 14:42:33 -0300

    Update

--- install.sh
+++ install.sh
@@ -1,45 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ $(id -u) -ne 0 ]];then
-	echo "Installing theme globally..."
-	sleep 1
-	global_install()
-else
+if [ "$(id -u)" -ne 0 ]; then
 	echo "Installing theme locally..."
 	sleep 1
-	local_install()
-fi
-
-function local_install() {
 	[ ! -d ~/.icons ] && mkdir ~/.icons
 	[ ! -d ~/.themes ] && mkdir ~/.themes
 
-	echo "Copying files to ~/.icons"
-	mkdir -p ~/.icons/MacOS-Gnome-Icons
-	echo "Copying files to ~/.themes"
 	mkdir -p ~/.themes/MacOS-Gnome-Theme
-
-	cp -R Custom-Icons/* ~/.icons/MacOS-Gnome-Icons
+	echo "Copying files to ~/.themes"
 	cp -R Custom-Theme/* ~/.themes/MacOS-Gnome-Theme
 
+	mkdir -p ~/.icons/MacOS-Gnome-Icons
+	echo "Copying files to ~/.icons"
+	cp -R Custom-Icons/* ~/.icons/MacOS-Gnome-Icons
+
 	echo "Changing buttons position"
 	gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
 
 	echo "Done!"
 	echo "You might need to restart Gnome for some changes to take effect."
-}
+else
+	echo "Installing theme globally..."
+	sleep 1
 
-function global_install() {
 	mkdir -p /usr/share/icons/MacOS-Gnome-Icons
-	mkdir -p /usr/share/themes/MacOS-Gnome-Theme
-
+	echo "Copying files to /usr/share/icons/MacOS-Gnome-Icons"
 	cp -R Custom-Icons/* /usr/share/icons/MacOS-Gnome-Icons
-	cp -R Custom-Theme/* /usr/share/themes/MacOS-Gnome-Icons
+
+	mkdir -p /usr/share/themes/MacOS-Gnome-Theme
+	echo "Copying files to /usr/share/icons/MacOS-Gnome-Theme"
+	cp -R Custom-Theme/* /usr/share/themes/MacOS-Gnome-Theme
 
 	echo "Changing buttons position"
 	gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
 
 	echo "Done!"
 	echo "You might need to restart Gnome for some changes to take effect."
-}
-
+fi
