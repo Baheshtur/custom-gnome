b7002fd7d71710d81925e02bbdc8712035c3eec5
Baheshtur <eudes.scar@gmail.com>
2017-08-04 14:18:41 -0300

    Add local and global installation
    If the user run the script as root, the theme is installed on /usr/share/ directory, globally.

--- install.sh
+++ install.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
-[ ! -d ~/.icons ] && mkdir ~/.icons
-[ ! -d ~/.themes ] && mkdir ~/.themes
+if [[ $(id -u) -ne 0 ]];then
+	echo "Installing theme globally..."
+	sleep 1
+	global_install()
+else
+	echo "Installing theme locally..."
+	sleep 1
+	local_install()
+fi
 
-echo "Copying files to ~/.icons"
-mkdir -p ~/.icons/MacOS-Gnome-Icons
-echo "Copying files to ~/.themes"
-mkdir -p ~/.themes/MacOS-Gnome-Theme
+function local_install() {
+	[ ! -d ~/.icons ] && mkdir ~/.icons
+	[ ! -d ~/.themes ] && mkdir ~/.themes
 
-cp -R Custom-Icons/* ~/.icons/MacOS-Gnome-Icons
-cp -R Custom-Theme/* ~/.themes/MacOS-Gnome-Theme
+	echo "Copying files to ~/.icons"
+	mkdir -p ~/.icons/MacOS-Gnome-Icons
+	echo "Copying files to ~/.themes"
+	mkdir -p ~/.themes/MacOS-Gnome-Theme
 
-echo "Changing buttons position"
-gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
+	cp -R Custom-Icons/* ~/.icons/MacOS-Gnome-Icons
+	cp -R Custom-Theme/* ~/.themes/MacOS-Gnome-Theme
 
-echo "Done!"
-echo "You might need to restart Gnome for some changes to take effect."
+	echo "Changing buttons position"
+	gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
+
+	echo "Done!"
+	echo "You might need to restart Gnome for some changes to take effect."
+}
+
+function global_install() {
+	mkdir -p /usr/share/icons/MacOS-Gnome-Icons
+	mkdir -p /usr/share/themes/MacOS-Gnome-Theme
+
+	cp -R Custom-Icons/* /usr/share/icons/MacOS-Gnome-Icons
+	cp -R Custom-Theme/* /usr/share/themes/MacOS-Gnome-Icons
+
+	echo "Changing buttons position"
+	gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
+
+	echo "Done!"
+	echo "You might need to restart Gnome for some changes to take effect."
+}
+
