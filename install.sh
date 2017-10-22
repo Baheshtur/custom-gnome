8108aee7b39635ef8abd548ced6c0c1fd3c6cf11
eudescar <eudes.scar@gmail.com>
2017-10-22 14:38:39 -0300

    Simplify install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 if [ "$(id -u)" -ne 0 ]; then
-	DEST_ICONS=~/.icons/
-	DEST_THEME=~/.themes/
+	DEST_ICONS=~/.icons
+	DEST_THEME=~/.themes
 	MSG="Installing theme locally..."
 else
-	DEST_ICONS=/usr/share/icons/
-	DEST_THEME=/usr/share/themes/
+	DEST_ICONS=/usr/share/icons
+	DEST_THEME=/usr/share/themes
 	MSG="Installing theme globally..."
 fi
 
 echo $MSG
 sleep 1
-[ ! -d $DEST_ICONS ] && mkdir -p $DEST_ICONS/MacOS-Gnome-Icons
-[ ! -d $DEST_THEME ] && mkdir -p $DEST_THEME/MacOS-Gnome-Theme
+
+mkdir -p $DEST_ICONS/MacOS-Gnome-Icons
+mkdir -p $DEST_THEME/MacOS-Gnome-Theme
 
 cp -R -v Custom-Icons/* $DEST_ICONS/MacOS-Gnome-Icons
 cp -R -v Custom-Theme/* $DEST_THEME/MacOS-Gnome-Theme
